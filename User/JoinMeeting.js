647ce2717a6fd38ef7bdbf3a3d7b0eb2ed113721
Saeed Nemati <sn.1361@gmail.com>
2021-11-03 00:22:50 +0330

    committed by script

--- User/JoinMeeting.js
+++ User/JoinMeeting.js
@@ -1,8 +1,81 @@
+import ZoomMtgEmbedded from "@zoomus/websdk/embedded";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { useState, useEffect } from 'react';
+import { get, app } from '@Panel';
 
+const client = ZoomMtgEmbedded.createClient();
 
 const JoinMeeting = () => {
+
+    const [meetingNumber, setMeetingNumber] = useState('');
+    const [password, setPassword] = useState('');
+    const [progress, setProgress] = useState(false);
+
+    useEffect(() => {
+        let meetingSDKElement = document.getElementById('meetingSDKElement');
+        client.init({
+            debug: true,
+            zoomAppRoot: meetingSDKElement,
+            language: 'en-US',
+            customize: {
+                meetingInfo: ['topic', 'host', 'mn', 'pwd', 'telPwd', 'invite', 'participant', 'dc', 'enctype'],
+                toolbar: {
+                    buttons: [
+                        {
+                            text: 'Custom Button',
+                            className: 'CustomButton',
+                            onClick: () => {
+                                console.log('custom button');
+                            }
+                        }
+                    ]
+                }
+            }
+        });
+    }, []);
+
+    const join = () => {
+        setProgress(true);
+        get(`/zoom/generateToken?meetingNumber=${meetingNumber}`).then(data => {
+            setProgress(false);
+            app.success('You are joining ...');
+            client.join({
+                apiKey: `${process.env.REACT_APP_ZOOM_API_KEY}`,
+                signature: data.signature,
+                meetingNumber: meetingNumber,
+                password: password,
+                userName: app.user()
+            })
+        }, error => {
+            setProgress(false);
+            app.error(error);
+        });
+    }
+
     return <div>
-        join meeting
+        <div id="meetingSDKElement">
+
+        </div>
+
+        <TextField
+            label={app.t('Meeting number')}
+            helperText='Required'
+            value={meetingNumber}
+            onChange={(e) => setMeetingNumber(e.target.value)}
+        />
+
+        <TextField
+            label={app.t('Password')}
+            helperText='Required'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+            variant="outlined"
+            onClick={() => join()}>
+            {app.t('Join')}
+        </Button>
     </div>
 }
 
